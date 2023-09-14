2f1c3ea2d241f1abeae35f137c45f1084f0fe8e1
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-09-14 14:05:39 -0300

    Capítulo 03
    Complete a tabela a seguir utilizando a = True, b = False e c = True.

---
+++ Exercicios/ex009.py
@@ -0,0 +1,18 @@
+a = True
+b = False
+c = True
+
+print(f'{"Expressão":<11} {"Resultado"}')
+print(f'{"a and a":<11} {a and a}')
+print(f'{"b and b":<11} {b and b}')
+print(f'{"not c":<11} {not c}')
+print(f'{"not b":<11} {not b}')
+print(f'{"not a":<11} {not a}')
+print(f'{"a and b":<11} {a and b}')
+print(f'{"b and c":<11} {b and c}')
+print(f'{"a or c":<11} {a or c}')
+print(f'{"b or c":<11} {b or c}')
+print(f'{"c or a":<11} {c or a}')
+print(f'{"c or b":<11} {c or b}')
+print(f'{"c or c":<11} {c or c}')
+print(f'{"b or b":<11} {b or b}')
