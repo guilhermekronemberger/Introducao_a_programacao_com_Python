05f05858f79fad217ed990bfefbe936e10e701e7
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-09-16 19:59:29 -0300

    Capítulo 03
    Calcule o resultado da expressão A > B and C or D, utilizando os valores da tabela a seguir.

---
+++ Exercicios/ex011.py
@@ -0,0 +1,18 @@
+print(f'{"A":^7}{"B":^7}{"C":^7}{"D":^7} Resultado')
+A = 1
+B = 2
+C = True
+D = False
+print(f'{A:^7}{B:^7}{C:^7}{D:^7} {A > B and C or D}')
+
+A = 10
+B = 3
+C = False
+D = False
+print(f'{A:^7}{B:^7}{C:^7}{D:^7} {A > B and C or D}')
+
+A = 5
+B = 1
+C = True
+D = True
+print(f'{A:^7}{B:^7}{C:^7}{D:^7} {A > B and C or D}')
