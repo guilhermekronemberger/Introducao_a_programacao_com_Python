81ad99e0f4205b2586f353facffe06ec087e842d
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-09-17 18:54:11 -0300

    Delete ex012.py

--- Exercicios/ex012.py
+++
@@ -1,8 +0,0 @@
-materia1 = float(input('Digite a nota da primeira matéria: '))
-materia2 = float(input('Digite a nota da segunda matéria: '))
-materia3 = float(input('Digite a nota da terceira matéria: '))
-
-if materia1 > 7 and materia2 > 7 and materia3 > 7:
-    print('Aprovado!')
-else:
-    print('Reprovado!')
