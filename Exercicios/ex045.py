ca95839bae4360bd3935eb317277ccfad94daddb
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-10-28 22:31:22 -0300

    Capítulo 05
    Escreva um programa para controlar uma pequena máquina registradora.
    Você deve solicitar ao usuário que digite o código do produto e a quantidade comprada.
    Utilize a tabela de códigos a seguir para obter o preço de cada produto: código 1 - 0.50, código 2 - 1.00, código 3 - 4.00, código 4 - 7.00, código 5 - 8.00.
    Seu programa deve exibir o total das compras depois que o usuário digitar 0. Qualquer outro código deve gerar a mensagem de erro “Código inválido”.

--- Exercicios/ex045.py
+++ Exercicios/ex045.py
@@ -7,20 +7,20 @@ while True:
     if codigo == 0:
         break
     elif codigo == 1:
-        preço = 0.50
+        preco = 0.50
     elif codigo == 2:
-        preço = 1.00
+        preco = 1.00
     elif codigo == 3:
-        preço = 4.00
+        preco = 4.00
+    elif codigo == 4:
+        preco = 7.00
     elif codigo == 5:
-        preço = 7.00
-    elif codigo == 9:
-        preço = 8.00
+        preco = 8.00
     else:
         print('ERRO! código inválido!')
 
-    if preço != 0:
+    if preco != 0:
         quantidade = int(input('Quantidade: '))
-        total = total + (preço * quantidade)
+        total = total + (preco * quantidade)
 
 print(f'Total a pagar R${total:.2f}')
