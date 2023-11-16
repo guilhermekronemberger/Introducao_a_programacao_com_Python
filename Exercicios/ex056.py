bc859acad9e28e15172cad327f9f30c5f0614395
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-11-16 20:04:02 -0300

    Capítulo 06
    Cálculo da média com notas digitadas.

---
+++ Exercicios/ex056.py
@@ -0,0 +1,18 @@
+notas = [0, 0, 0, 0, 0, 0, 0]
+soma = 0
+contador = 0
+
+print()
+while contador < len(notas):
+    notas[contador] = float(input(f'Nota {contador}: '))
+    soma += notas[contador]
+    contador += 1
+
+contador = 0
+
+print()
+while contador < len(notas):
+    print(f'Nota {contador}: {notas[contador]:.1f}')
+    contador += 1
+
+print(f'Média: {soma / len(notas):.1f}')
